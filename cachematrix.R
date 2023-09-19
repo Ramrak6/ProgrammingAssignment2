0fb6bc59b7fbc11d44cf8c02b23c402a2696008b
Ramrak6 <141828891+Ramrak6@users.noreply.github.com>
2023-09-19 11:08:41 +0100

    Week 3 assignements
    Anas

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<- NULL    #initializing inverse as null
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
 }
 
+  get <- function() x    #function to get matrix
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() {
+    inver<- ginv(x)
+    inver%%x    #function to obtain inverse of the matrix
+  }
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This is used to get the cache data
+cacheSolve <- function(x, ...) ## gets cache data
+{ 
+  inv<- x$getinv()
+  if(!is.null(inv)){ #checking wheater inverse is null
+    message("getting cached data!")
+    return(inv)
+  }
+    data<- x$get()
+    inv<- slove(data,...) #calculates inverse value
+    x$setinv(inv)
+     inv   ## Return a matrix that is the inverse of 'x'
 }
